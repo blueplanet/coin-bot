2bc1b122f8683231b1292f646423082383ccf51e
blueplanet <gyo@re-work.io>
2018-02-09 14:23:13 +0900

    ruby 2.5.0

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,11 @@
-class EventsController < ApplicationController
+class EventsController < ActionController::API
+
   def create
     challenge = params[:challenge]
 
     case params[:type]
     when 'url_verification'
       render json: { challenge: params[:challenge] }
-    when 'event_callback'
     end
   end
 end
