780fd6a4a19576bf5d9da534b1f635f827419df1
blueplanet <gyo@re-work.io>
2018-02-14 06:31:57 +0900

    一時実装

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,5 +7,12 @@ class EventsController < ActionController::API
     when 'url_verification'
       render json: { challenge: params[:challenge] }
     end
+
+    client = Ethereum::HttpClient.new("https://ropsten.infura.io/#{ENV['INFURA_TOKEN']}")
+    key = Eth::Key.new(priv: ENV['ERC20_OWNER_PRIVATE_KEY'])
+    abi = JSON.parse(File.open(Rails.root.join('config', 'abi', 'erc20_token.json')).read)
+    contract = Ethereum::Contract.create(client: client, name: "ERC20Token", address: ENV['ERC20_ADDRESSES'], abi: abi)
+    contract.key = key
+    contract.transact.transfer('to_address', 5e18.to_i)
   end
 end
