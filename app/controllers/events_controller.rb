96717bd768f887a7f6a0c6bddb53c52044b11d00
blueplanet <gyo@re-work.io>
2018-02-14 10:15:41 +0900

    非同期実行

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,42 +7,25 @@ class EventsController < ActionController::API
     when 'url_verification'
       render json: { challenge: params[:challenge] }
     when 'reaction_added'
-      bot = Slack::Web::Client.new
-      slack_user = SlackUser.find_by(
-        team_id: params[:team_id], 
-        api_app_id: params[:api_app_id], 
-        user_id: params[:event][:item_user]
+      ReactionAddedJob.perform_later(
+        params[:team_id],
+        params[:api_app_id],
+        params[:event][:item_user],
+        params[:event][:item][:channel]
       )
-
-      if slack_user
-        coin = 5
-
-        client = Ethereum::HttpClient.new("https://ropsten.infura.io/#{ENV['INFURA_TOKEN']}")
-        key = Eth::Key.new(priv: ENV['ERC20_OWNER_PRIVATE_KEY'])
-        abi = JSON.parse(File.open(Rails.root.join('config', 'abi', 'erc20_token.json')).read)
-        contract = Ethereum::Contract.create(client: client, name: "ERC20Token", address: ENV['ERC20_ADDRESSES'], abi: abi)
-        contract.key = key
-
-        transaction = contract.transact.transfer(slack_user.address, coin * 10**18)
-
-        message = "#{coin} MOF 頂きました〜\nhttps://ropsten.etherscan.io/tx/#{transaction.id}"
-
-        bot.chat_postMessage(
-          as_user: 'true',
-          channel: params[:event][:item][:channel],
-          text: message
-        )
-      else
-        bot.chat_postMessage(
-          as_user: 'true',
-          channel: params[:event][:item][:channel],
-          text: "アドレスはまだ登録されていません。\n`@bot register ADDRESS`を実行して登録しましょう！"
-        )
-      end
 
       head :ok
     else
       head :ok
     end
   end
+
+  private
+
+    def reaction_added_params
+      params.permit(
+        :team_id, :api_app_id,
+        event: [:type, :item_user, item: :channel]
+      )
+    end
 end
---
+++ app/jobs/reaction_added_job.rb
@@ -0,0 +1,42 @@
+class ReactionAddedJob < ApplicationJob
+  queue_as :default
+
+  COINS = 5
+
+  def perform(team_id, api_app_id, user_id, channel)
+    bot = Slack::Web::Client.new
+
+    slack_user = SlackUser.find_by(
+      team_id: team_id, 
+      api_app_id: api_app_id, 
+      user_id: user_id
+    )
+
+    if slack_user
+      transaction = send_coin(slack_user.address)
+
+      message = "#{COINS} MOF 送金しましたよ〜\nhttps://ropsten.etherscan.io/tx/#{transaction.id}"
+
+      bot.chat_postMessage(as_user: 'true', channel: channel, text: message)
+    else
+      bot.chat_postMessage(
+        as_user: 'true',
+        channel: channel,
+        text: "アドレスはまだ登録されていません。\n`@bot register ADDRESS`を実行して登録しましょう！"
+      )
+    end
+
+  end
+
+  private
+
+    def send_coin(address)
+      client = Ethereum::HttpClient.new("https://ropsten.infura.io/#{ENV['INFURA_TOKEN']}")
+      key = Eth::Key.new(priv: ENV['ERC20_OWNER_PRIVATE_KEY'])
+      abi = JSON.parse(File.open(Rails.root.join('config', 'abi', 'erc20_token.json')).read)
+      contract = Ethereum::Contract.create(client: client, name: "ERC20Token", address: ENV['ERC20_ADDRESSES'], abi: abi)
+      contract.key = key
+
+      contract.transact.transfer(address, COINS * 10**18)
+    end
+end
