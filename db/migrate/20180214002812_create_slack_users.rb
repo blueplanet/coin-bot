afbf4690ff3255b15c289568f49f4e3b8ced76d6
blueplanet <gyo@re-work.io>
2018-02-14 09:44:47 +0900

    Add slack user

---
+++ db/migrate/20180214002812_create_slack_users.rb
@@ -0,0 +1,14 @@
+class CreateSlackUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :slack_users do |t|
+      t.string :team_id
+      t.string :api_app_id
+      t.string :user_id
+      t.string :address
+
+      t.timestamps
+    end
+
+    add_index :slack_users, %i[team_id api_app_id user_id], unique: true
+  end
+end
