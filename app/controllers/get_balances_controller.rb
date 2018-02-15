0a9a56aaccea74f20541b97db1d1be211be0232a
blueplanet <gyo@re-work.io>
2018-02-15 09:37:00 +0900

    Fix typo

--- app/controllers/get_balances_controller.rb
+++ app/controllers/get_balances_controller.rb
@@ -1,6 +1,6 @@
 class GetBalancesController < ActionController::API
   def create
-    GetBalanceJob.peform_later(params[:team_id], params[:user_id])
+    GetBalanceJob.perform_later(params[:team_id], params[:user_id])
 
     head :ok
   end
