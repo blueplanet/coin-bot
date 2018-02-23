19d4142db78889288b4dc1dd0406dd5dd27911e8
blueplanet <gyo@re-work.io>
2018-02-23 19:48:21 +0900

    Fix key

--- lib/token_contract.rb
+++ lib/token_contract.rb
@@ -10,10 +10,11 @@ class TokenContract
   attr_reader :contract
 
   def initialize
-    client = Ethereum::HttpClient.new("https://ropsten.infura.io/#{ENV['INFURA_TOKEN']}")
+    client = Ethereum::Client.create("https://ropsten.infura.io/#{ENV['INFURA_TOKEN']}")
     key = Eth::Key.new(priv: ENV['ERC20_OWNER_PRIVATE_KEY'])
     abi = JSON.parse(File.open(Rails.root.join('config', 'abi', 'erc20_token.json')).read)
     @contract = Ethereum::Contract.create(client: client, name: "ERC20Token", address: ENV['ERC20_ADDRESSES'], abi: abi)
+    @contract.key = key
     @contract.sender = key.address
   end
 end
