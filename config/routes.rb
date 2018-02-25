f10985581c6cd2b4037dd03bc513979a30d1f1f9
blueplanet <gyo@re-work.io>
2018-02-26 05:59:37 +0900

    Add transactions new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'transactions/new'
+
   resources :events, only: :create, formats: :json
   resources :get_balances, only: :create
   resources :slack_users, only: :create, formats: :json
