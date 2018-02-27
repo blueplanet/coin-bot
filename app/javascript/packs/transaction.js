6edd6d141a36bc1d1c7c8b1b2a9fa3c6e41805ce
blueplanet <gyo@re-work.io>
2018-02-28 07:08:42 +0900

    Add transaction.js

---
+++ app/javascript/packs/transaction.js
@@ -0,0 +1,22 @@
+window.addEventListener('load', function() {
+  if (typeof web3 !== 'undefined') {
+    // Use Mist/MetaMask's provider
+    web3js = new Web3(web3.currentProvider);
+
+    web3.version.getNetwork((err, netId) => {
+      switch (netId) {
+        case "2":
+          console.log('This is the ropsten test network.')
+          break
+        default:
+          alert('現在 MOF コインは Ropsten で稼働しているので、MetaMaskで Ropsten ネットワークを選択してください。');
+          location.href = '/';
+      }
+    })
+  } else {
+    alert('MetaMask をインストールしてください');
+
+    location.href = '/';
+  }
+
+});
