6a1fbfe3d4ccbb1fe756fcbb674a2194f7c3c9c5
blueplanet <gyo@re-work.io>
2018-02-28 09:04:11 +0900

    Add submit

--- app/javascript/packs/transaction.js
+++ app/javascript/packs/transaction.js
@@ -1,22 +1,29 @@
 window.addEventListener('load', function() {
   if (typeof web3 !== 'undefined') {
-    // Use Mist/MetaMask's provider
-    web3js = new Web3(web3.currentProvider);
-
     web3.version.getNetwork((err, netId) => {
-      switch (netId) {
-        case "3":
-          console.log('This is the ropsten test network.')
-          break
-        default:
-          alert('現在 MOF コインは Ropsten で稼働しているので、MetaMaskで Ropsten ネットワークを選択してください。');
-          location.href = '/';
+      if (netId !== "3") {
+        alert('現在 MOF コインは Ropsten で稼働しているので、MetaMaskで Ropsten ネットワークを選択してください。');
+        location.href = '/';
       }
-    })
+    });
+
+    let defaultAccount = web3.eth.accounts[0];
+    if (typeof defaultAccount !== 'undefined') {
+      console.log('Use account: [' + defaultAccount + ']');
+    } else {
+      alert('MetaMask をアンロックしてください。')
+      return;
+    }
+
+    form = document.getElementById("transaction-form");
+    form.addEventListener('submit', function(e) {
+      e.preventDefault();
+      
+      
+    });
   } else {
     alert('MetaMask をインストールしてください');
 
     location.href = '/';
   }
-
 });
