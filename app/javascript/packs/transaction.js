d75cfb1959fc4b76fa1c4aab199b2365a020a1f7
blueplanet <gyo@re-work.io>
2018-03-01 08:43:14 +0900

    Add ethjs

--- app/javascript/packs/transaction.js
+++ app/javascript/packs/transaction.js
@@ -1,25 +1,15 @@
+const Eth = require('ethjs');
+const unit = require('ethjs-unit');
+const eth = new Eth(new Eth.HttpProvider('https://ropsten.infura.io'));
+const erc20ABI = [{ "constant": true, "inputs": [], "name": "name", "outputs": [ { "name": "", "type": "string" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "_spender", "type": "address" }, { "name": "_value", "type": "uint256" } ], "name": "approve", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "totalSupply", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "_from", "type": "address" }, { "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" } ], "name": "transferFrom", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "decimals", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "_spender", "type": "address" }, { "name": "_subtractedValue", "type": "uint256" } ], "name": "decreaseApproval", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [ { "name": "_owner", "type": "address" } ], "name": "balanceOf", "outputs": [ { "name": "balance", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "symbol", "outputs": [ { "name": "", "type": "string" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" } ], "name": "transfer", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "_spender", "type": "address" }, { "name": "_addedValue", "type": "uint256" } ], "name": "increaseApproval", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [ { "name": "_owner", "type": "address" }, { "name": "_spender", "type": "address" } ], "name": "allowance", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "inputs": [ { "name": "initialSupply", "type": "uint256" } ], "payable": false, "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [ { "indexed": true, "name": "owner", "type": "address" }, { "indexed": true, "name": "spender", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" } ], "name": "Approval", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "name": "from", "type": "address" }, { "indexed": true, "name": "to", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" } ], "name": "Transfer", "type": "event" } ];
+
 window.addEventListener('load', function() {
-  if (typeof web3 !== 'undefined') {
-    web3.version.getNetwork((err, netId) => {
-      if (netId !== "3") {
-        alert('現在 MOF コインは Ropsten で稼働しているので、MetaMaskで Ropsten ネットワークを選択してください。');
-        location.href = '/';
-      }
-    });
+  if (typeof window.web3 !== 'undefined' && typeof window.web3.currentProvider !== 'undefined') {
+    eth.setProvider(window.web3.currentProvider);
+    const token = eth.contract(erc20ABI).at('0x93F42602d8448d81E426FB2C38369179790c3E4A');
 
-    let defaultAccount = web3.eth.accounts[0];
-    if (typeof defaultAccount !== 'undefined') {
-      console.log('Use account: [' + defaultAccount + ']');
-    } else {
-      alert('MetaMask をアンロックしてください。')
-      return;
-    }
-
-    form = document.getElementById("transaction-form");
-    form.addEventListener('submit', function(e) {
-      e.preventDefault();
-      
-      web3.
+    token.totalSupply().then((totalSupply) => {
+      console.log(unit.fromWei(totalSupply[0], 'ether'));
     });
   } else {
     alert('MetaMask をインストールしてください');
