17054593fca04ccf8b4008d78a932ec17977ce7c
blueplanet <gyo@re-work.io>
2018-03-01 09:50:28 +0900

    MOF残高表示

--- app/javascript/packs/transaction.js
+++ app/javascript/packs/transaction.js
@@ -2,18 +2,29 @@ const Eth = require('ethjs');
 const unit = require('ethjs-unit');
 const eth = new Eth(new Eth.HttpProvider('https://ropsten.infura.io'));
 const erc20ABI = [{ "constant": true, "inputs": [], "name": "name", "outputs": [ { "name": "", "type": "string" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "_spender", "type": "address" }, { "name": "_value", "type": "uint256" } ], "name": "approve", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "totalSupply", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "_from", "type": "address" }, { "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" } ], "name": "transferFrom", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "decimals", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "_spender", "type": "address" }, { "name": "_subtractedValue", "type": "uint256" } ], "name": "decreaseApproval", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [ { "name": "_owner", "type": "address" } ], "name": "balanceOf", "outputs": [ { "name": "balance", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "symbol", "outputs": [ { "name": "", "type": "string" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" } ], "name": "transfer", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "_spender", "type": "address" }, { "name": "_addedValue", "type": "uint256" } ], "name": "increaseApproval", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [ { "name": "_owner", "type": "address" }, { "name": "_spender", "type": "address" } ], "name": "allowance", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "inputs": [ { "name": "initialSupply", "type": "uint256" } ], "payable": false, "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [ { "indexed": true, "name": "owner", "type": "address" }, { "indexed": true, "name": "spender", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" } ], "name": "Approval", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "name": "from", "type": "address" }, { "indexed": true, "name": "to", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" } ], "name": "Transfer", "type": "event" } ];
+const mofCoinAddress = '0x93F42602d8448d81E426FB2C38369179790c3E4A';
 
 window.addEventListener('load', function() {
   if (typeof window.web3 !== 'undefined' && typeof window.web3.currentProvider !== 'undefined') {
     eth.setProvider(window.web3.currentProvider);
-    const token = eth.contract(erc20ABI).at('0x93F42602d8448d81E426FB2C38369179790c3E4A');
+    window.eth = eth;
 
-    token.totalSupply().then((totalSupply) => {
-      console.log(unit.fromWei(totalSupply[0], 'ether'));
+    const token = eth.contract(erc20ABI).at(mofCoinAddress);
+
+    eth.accounts().then((accounts) => {
+      window.eth.defaultAccount = accounts[0];
+      this.document.querySelector('#from-address').value = eth.defaultAccount;
+
+      token.balanceOf(eth.defaultAccount).then((balance) => {
+        document.querySelector('#balance').textContent = unit.fromWei(balance[0], 'ether');
+      });
     });
+
   } else {
     alert('MetaMask をインストールしてください');
 
     location.href = '/';
   }
 });
+
+// TODO: アカウント変更監視
